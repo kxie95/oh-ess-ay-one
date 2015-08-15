b7f8865c623bcf4b004398fc2c96b2fb2ddcc889
kxie094 <kxie094@aucklanduni.ac.nz>
2015-08-15 13:53:39 +1200

    Finished wait. All functions for background finished. Move on to interactive now.

--- dispatcher.py
+++ dispatcher.py
@@ -16,6 +16,7 @@ class Dispatcher():
     def __init__(self):
         """Construct the dispatcher."""
         self.runnable_stack = []
+        self.waiting_stack = []
         self.event = Event()
 
     def set_io_sys(self, io_sys):
@@ -44,7 +45,7 @@ class Dispatcher():
         if len(stack) == 1:
             return
 
-        stack[-1].event.clear() # stop current process
+        stack[-1].event.clear()  # stop current process
         original_pos = stack.index(process)
         self.io_sys.move_process(stack[original_pos], len(stack))
 
@@ -56,7 +57,6 @@ class Dispatcher():
             self.io_sys.move_process(stack[x], x)
 
         process.event.set()
-
 
     def pause_system(self):
         """Pause the currently running process.
@@ -71,13 +71,18 @@ class Dispatcher():
 
     def wait_until_finished(self):
         """Hang around until all runnable processes are finished."""
-        # ...
+        while True:
+            if len(self.runnable_stack) == 0:
+                return
 
     def proc_finished(self, process):
         """Receive notification that "proc" has finished.
         Only called from running processes.
         """
-        # ...
+        if not len(self.runnable_stack) == 0:
+            self.runnable_stack.pop()
+            self.io_sys.remove_window_from_process(process)
+            self.dispatch_next_process()
 
     def proc_waiting(self, process):
         """Receive notification that process is waiting for input."""
@@ -91,10 +96,10 @@ class Dispatcher():
         return None
 
     def kill_process(self, process):
-        process.event.clear() 
-        self.runnable_stack.remove(process) # remove desired process from stack
-        self.io_sys.remove_window_from_process(process) # update display to reflect remove
-
+        process.event.clear()
+        self.runnable_stack.remove(process)  # remove desired process from stack
+        self.io_sys.remove_window_from_process(process)  # update display to reflect remove
+        process.state = State.killed
         # shift processes down
         for x in range(0, len(self.runnable_stack)):
             self.io_sys.move_process(self.runnable_stack[x], x)
